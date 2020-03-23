1896e80be836c5cdbd32888035d0a708355a8cfb
NadiaCarvalho <nadiacarvalho118@gmail.com>
2020-03-23 00:22:59 +0000

    cleaning more

--- generation_system/example_microtons.py
+++ generation_system/example_microtons.py
@@ -7,12 +7,12 @@ import representation.parser as parser
 
 
 if __name__== "__main__":
-    file_path = os.sep.join(['myexamples','MicrotonsExample.mxl'])
+    file_path = os.sep.join(['myexamples','bwv1.6.mxl'])
     if os.path.realpath('.').find('generation_system') == -1:
         file_path = os.sep.join(['generation_system', file_path]) 
 
     example = converter.parse(file_path)
     #example.flat.show('text')
-    events = parser.parseLine(example)
+    events = parser.parseLine(example.parts[0])
     
-    print(utils.showSequenceOfViewpointWithoutOffset(events, 'repeat_direction'))
+    print(utils.showSequenceOfViewpointWithoutOffset(events, 'midi_pitch'))
