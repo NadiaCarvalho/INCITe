f2d8b7b2585fd334a6fb3346301e7e81e189455e
NadiaCarvalho <nadiacarvalho118@gmail.com>
2020-03-24 22:55:15 +0000

    linting rules

--- generation_system/code/main.py
+++ generation_system/code/main.py
@@ -5,14 +5,14 @@ from music21 import converter, corpus
 import representation.utils as utils
 from representation.parser import LineParser
 
-if __name__== "__main__":
-    file_path = os.sep.join(['myexamples','MicrotonsExample.mxl'])
+if __name__ == "__main__":
+    file_path = os.sep.join(['myexamples', 'MicrotonsExample.mxl'])
     if os.path.realpath('.').find('code') != -1:
         file_path.replace('code', '')
-        file_path = os.sep.join(['..', file_path]) 
+        file_path = os.sep.join(['..', file_path])
 
     example = converter.parse(file_path)
-    #example.flat.show('text')
+    # example.flat.show('text')
     events = LineParser(example.parts[0]).parseLine()
-    
+
     print(utils.showSequenceOfViewpointWithoutOffset(events, 'timesig'))
