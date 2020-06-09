b660069b94a9447c1e25d7a1f1617bd243d4dabb
NadiaCarvalho <nadiacarvalho118@gmail.com>
2020-06-10 00:12:36 +0100

    now image is on both app executable and console

--- generation_system/code/application/interface/main_window.py
+++ generation_system/code/application/interface/main_window.py
@@ -18,7 +18,8 @@ def resource_path(relative_path):
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        base_path = os.sep.join(['.', 'data', 'images'])
+        base_path = os.path.abspath(base_path)
 
     return os.path.join(base_path, relative_path)
 
